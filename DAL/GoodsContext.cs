a83217020ed491c94523f592fe0488aee112d15b
farfander <farfander@mail.ru>
2022-12-11 15:09:13 +0400

    settings of goods

---
+++ DAL/GoodsContext.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLibrary.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary
+{
+    public class GoodsContext : DbContext
+    {
+        
+        public DbSet<Goods> goods { get; set; }
+        public DbSet<Department> departments { get; set; }
+        public DbSet<Sale> sales { get; set; }
+        public DbSet<Supplie> supplies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\GSSDB.mdf;Integrated Security=True;Connect Timeout=30");
+    }
+}
