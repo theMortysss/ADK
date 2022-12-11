4e30027def09c344c79bbda426ed8c0863559387
Artemka <art03po@mail.ru>
2022-12-11 15:00:29 +0400

    entities of project

---
+++ DAL/models/Sale.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLibrary.models
+{
+    public class Sale
+    {
+        [Column("id")] public int Id { get; set; }
+        [Column("good_id")] public int GoodId { get; set; }
+
+        [Column("good_count")] public int SoldAmount { get; set; }
+
+        [Column("date_sold")] public DateTime DateSold { get; set; }
+        public Goods Good { get; set; }
+
+        public int RemainingAmount => Good.AvailableAmount;
+        public string GoodName => Good.Name;
+        public decimal GoodPrice => Good.Price;
+
+        public override string ToString()
+        {
+            return $"[{DateSold}] {Good}: {SoldAmount}";
+        }
+
+        public Sale()
+        {
+        }
+
+        public Sale(Goods good, int soldAmount, DateTime dateSold)
+        {
+            Good = good;
+            GoodId = good.Id;
+            SoldAmount = soldAmount;
+            DateSold = dateSold;
+        }
+    }
+}
---
+++ DAL/models/Supplie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLibrary.models
+{
+    // [Table("supplies")]
+    public class Supplie
+    {
+        [Column("id")] public int Id { get; set; }
+        [Column("good_id")] public int GoodId { get; set; }
+
+        [Column("date_supplied")] public DateTime DateSupplied { get; set; }
+
+        [Column("date_sold")] public DateTime? DateSold { get; set; }
+        [Column("remaining_qty")] public int RemainingQuantity { get; set; }
+
+        public Goods Good { get; set; }
+
+        [NotMapped] public bool IsExpired => DateSupplied.AddDays(Good.ExpirationDaysCount) < DateTime.Now;
+
+        public string GoodName => Good.Name;
+
+        public Supplie()
+        {
+        }
+
+        public Supplie(Goods good, int amount, DateTime dateSupplied)
+        {
+            Good = good;
+            RemainingQuantity = amount;
+            DateSupplied = dateSupplied;
+        }
+    }
+}
