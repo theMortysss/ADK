d1fda706ae0c2921ee5794b18b79be53ff234734
farfander <farfander@mail.ru>
2022-12-11 16:15:52 +0400

    GUI of storage and logik of storage

---
+++ ADK/StorageWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GSS
+{
+    /// <summary>
+    /// Interaction logic for StorageWindow.xaml
+    /// </summary>
+    public partial class StorageWindow : Window
+    {
+        public StorageWindow()
+        {
+            InitializeComponent();
+            using(var db = new GoodsContext())
+            {
+                var goodsList = db.goods.ToList();
+                var supplies = db.supplies.ToList();
+                goodsGrid.ItemsSource = goodsList;
+            }
+        }
+    }
+}
